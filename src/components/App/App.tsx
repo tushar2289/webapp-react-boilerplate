d3ed7008226bdab7e54aa10cffcf3577d4060b29
Tushar Sahoo <tushar.sahoo@rksv.in>
2020-06-24 00:40:22 +0530

    Custom theme added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+import DefaultTheme from '../../themes/default';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,23 +30,33 @@ const useStyles = makeStyles((theme: Theme) =>
 const App: React.FC = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <ThemeProvider theme={DefaultTheme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              Material UI
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="md">
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Create React App Example with Typescript and Material UI
+            </Typography>
+          </Box>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 };
 
